b27d9680c0daf9747c904e4b2f077d538ea485bd
Nicholas Sjöstrand <kaigan.codes@aol.com>
2024-11-01 14:36:28 +0100

    added functions.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// Performing a task
+function greet(name, lastName) {
+    console.log('Hello ' + name + ' ' + lastName);
+}
+
+greet('Nicholas', 'Sjöstrand');
+
+// Calculating a value.
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
+console.log(square(2));
