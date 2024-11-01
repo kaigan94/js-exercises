a801acbb718d7335f495265d4f8c70d5ff1cbaac
Nicholas Sjöstrand <kaigan.codes@aol.com>
2024-11-01 14:52:54 +0100

    added operators.

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ function square(number) {
 let number = square(2);
 console.log(number);
 console.log(square(2));
+
+// Operators
+let x = 10;
+let y = 3;
+
+// Expression in JS, something that produces a value.
+// console.log(x + y); // addition
+// console.log(x - y); // subtraction
+// console.log(x * y); // multiplication
+// console.log(x / y); // division
+// console.log(x % y); // remainder of division
+// console.log(x ** y); // NEW IN JS! "X" to the power of "Y"
+
+// Increment (++) 
+// console.log(x++);
+// console.log(x);
+
+// Decrement (--)
+// console.log(--x);
